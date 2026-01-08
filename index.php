1c0ef0ec520a65601efab75892ff503ac3bdbad7
alhaan-org <alhaanahmed@nexiatech.org>
2026-01-08 21:54:28 +0500

    feat: Refactor

--- index.php
+++ index.php
@@ -14,21 +14,19 @@
       href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css"
       rel="stylesheet"
     />
-
+    <link
+    rel="stylesheet"
+    href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
+    />
+    <link
+    rel="stylesheet"
+    href="css/styles.css"
+    />
     <script
       src="https://code.jquery.com/jquery-3.7.1.js"
       integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4="
       crossorigin="anonymous"
     ></script>
-
-    <link
-      rel="stylesheet"
-      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
-    />
-    <link
-      rel="stylesheet"
-      href="../css/styles.css"
-    />
     <script src="js/app.js"></script>
   </head>
 

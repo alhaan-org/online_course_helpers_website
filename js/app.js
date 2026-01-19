2c0ec013ad3bccdccb4f003081ef885cd1a83dd8
alhaan-org <alhaanahmed@nexiatech.org>
2026-01-19 22:07:06 +0500

    feat: All Sections DOne, Animation left

--- js/app.js
+++ js/app.js
@@ -1,52 +1,6 @@
+
 $(document).ready(function () {
-  const $slider = $(".slider-images");
-  const $horizontalScrollWrapper = $(".horizontal-scroll-wrapper");
-  const $horizontalScrollCards = $(".course-card");
-  let isDown = false;
-  let startX;
-  let scrollLeft;
-
-  $slider.on("mousedown", function (e) {
-    isDown = true;
-    $slider.addClass("active");
-    startX = e.pageX - $slider.offset().left;
-    scrollLeft = $slider.scrollLeft();
-  });
-
-  $(document).on("mouseup mouseleave", function () {
-    isDown = false;
-    $slider.removeClass("active");
-  });
-
-  $slider.on("mousemove", function (e) {
-    if (!isDown) return;
-    e.preventDefault();
-
-    const x = e.pageX - $slider.offset().left;
-    const walk = (x - startX) * 2;
-    $slider.scrollLeft(scrollLeft - walk);
-  });
-
-  
-  $horizontalScrollCards.on("click", () => {
-    let direction = 1;
-    const value = 300;
-    const currentScroll = $horizontalScrollWrapper.scrollLeft();
-  
-    const maxScroll = $horizontalScrollWrapper[0].scrollWidth - $horizontalScrollWrapper.outerWidth();
-
-   
-    if (currentScroll >= maxScroll - 5)
-        direction = -1; 
-  
-    else if (currentScroll <= 0) {
-        direction = 1; 
-    }
-
-    $horizontalScrollWrapper.animate({
-        scrollLeft: currentScroll + (value * direction)
-    }, 300);
-  });
+//  Main Functionality Block
 
   $(window).scroll(function () {
     if ($(this).width() > 992) {

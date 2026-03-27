d483d09ec084e7aa6abafe853b8d56e132c1702e
alhaan-org <alhaanahmed@nexiatech.org>
2026-03-27 23:56:02 +0500

    feat: Head links update with dynamic canonical link and header.php with updated

--- include/head_links.php
+++ include/head_links.php
@@ -4,10 +4,12 @@
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css"
     integrity="sha512-2SwdPD6INVrV/lHTZbO2nodKhrnDdJK9/kg2XD1r9uGqPo1cUbujc+IYdlYdEErWNu69gVcYgdxlmVmzTWnetw=="
     crossorigin="anonymous" referrerpolicy="no-referrer" />
+<link rel="icon" type="img/x-icon" href="assets/img/favicon.png" />
 <link rel="stylesheet" href="assets/css/styles.css" />
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@12/swiper-bundle.min.css" />
 <link rel="stylesheet" href="assets/owlcarousel/owl.carousel.min.css">
 <link rel="stylesheet" href="assets/owlcarousel/owl.theme.default.min.css">
+
 <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
 <script src="https://code.jquery.com/jquery-3.7.1.js" integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4="
     crossorigin="anonymous">
--- include/header.php
+++ include/header.php
@@ -1,4 +1,8 @@
 <head>
+    <?php
+    $protocol = ((!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'off') || $_SERVER['SERVER_PORT'] == 443) ? "https://" : "http://";
+    $current_url = $protocol . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+    ?>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title><?php echo isset($pageTitle) ? $pageTitle : 'Guaranteed Extra Credits with Best Online Course Help'; ?>
@@ -10,10 +14,10 @@
     <meta property="og:description"
         content="<?php echo isset($pageDescription) ? $pageDescription : 'The most reliable online course help in USA to fast-track your route to extra credits. Hire professional online course helpers for timely submissions.' ?>">
     <meta property="og:type" content="Website">
-    <!-- URL and Image in Header -->
-    <!-- <meta property="og:url" content="https://academiceditingpros.co.uk"> -->
-    <!-- <meta property="og:image" content="https://academiceditingpros.co.uk/assets/images/logo.webp" > -->
-    <meta name="theme-color" content="#003d4d">
+    <meta property="og:url" content="https://onlinecoursehelpers.com">
+    <meta property="og:image" content="https://onlinecoursehelpers.com/assets/img/favicon.png">
+    <meta name="theme-color" content="#002a4d">
+    <link rel="canonical" href="<?php echo $current_url; ?>">
     <!-- Scripts and CSS Files -->
     <?php include 'head_links.php' ?>
 </head>
